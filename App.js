ea1d9525bd039612ea6fd34a927ffb1e54261694
NicolasDepre <niclas.depre@student.uclouvain.be>
2021-03-01 15:14:08 +0100

    First trace

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import MapView, { Polyline } from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions, Alert } from 'react-native';
+import { useState } from 'react';
+import getRoad from './ev3';
 
+
+var stateTest = {
+  strokeWidth : 50,
+  strokeColor : 'black'
+}
 export default function App() {
+
+  console.log(getRoad()[0])
+  const myRoad = getRoad()
+
+  const [width,setWidth] = useState({
+    strokeColor : 'black',
+    width : 2
+  })
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} >
+        <Polyline onPress={()=>setWidth(
+          {width : 5,
+          strokeColor:'pink'})} 
+          coordinates={myRoad}
+    strokeWidth={width.width}
+    strokeColor= {width.strokeColor}
+    />
+      </MapView>
     </View>
   );
 }
@@ -18,4 +41,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
 });
+
+
+
