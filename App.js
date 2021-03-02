302a797bde959d9439b43332f84bf7c21a815c99
Corentin Lingier <corentin.lingier@student.uclouvain.be>
2021-03-02 14:15:37 +0100

    navigation: initial commit

--- App.js
+++ App.js
@@ -4,12 +4,23 @@ import { StyleSheet, Text, View, Dimensions, Alert } from 'react-native';
 import { useState } from 'react';
 import getRoad from './ev3';
 import HomeScreen from './src/screens/HomeScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   return(
-    <HomeScreen/>
+    <>
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Home" component={HomeScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </>
   );
 };
+
+
 
 export default App
