d02152831fcf099f155469ce62b30feaa8b19463
Corentin Lingier <corentin.lingier@student.uclouvain.be>
2021-04-18 22:17:59 +0200

    design roads

--- App.js
+++ App.js
@@ -1,23 +1,11 @@
 import * as React from 'react';
 import HomeScreen from './src/screens/HomeScreen';
-import RoadsScreen from './src/screens/RoadsScreen';
-import Description from './src/screens/Description';
+import Roads from './src/screens/RoadsScreen';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import {createStackNavigator} from "@react-navigation/stack";
 
 const Tab = createBottomTabNavigator();
-const Stack = createStackNavigator();
-
-function Roads() {
-    return (
-        <Stack.Navigator>
-            <Stack.Screen name="Roads" component={RoadsScreen} />
-            <Stack.Screen name="Description" component={Description} />
-        </Stack.Navigator>
-    );
-}
 
 export default class App extends React.Component {
   constructor(props) {
--- src/screens/RoadsScreen/index.js
+++ src/screens/RoadsScreen/index.js
@@ -1,8 +1,96 @@
 import * as React from 'react';
-import {Alert, Button, StyleSheet, ScrollView, Image, Text, View} from 'react-native';
+import Description from '../Description';
+import {Dimensions, StyleSheet, ScrollView, Image, Text, View} from 'react-native';
 import pic from '../../assets/images/ev.jpg';
 import {TouchableHighlight} from 'react-native-gesture-handler';
 import routes from '../../assets/roads_data/roads.json';
+import {createStackNavigator} from "@react-navigation/stack";
+import MaskedView from '@react-native-community/masked-view';
+
+const Stack = createStackNavigator();
+
+/*
+ECRAN AVEC TOUTES LES ROUTES <3
+*/
+
+const scaleFactor = 1920 / Dimensions.get('window').width
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: '2%',
+        overflow:"visible",
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#FAFCFF',
+        width: '100%',
+    },
+
+    title: {
+        marginTop: 46,
+        fontSize: 24,
+        color: '#3D3F43'
+    },
+
+    velo: {
+        color: '#17AC40',
+        fontWeight: 'bold'
+    },
+
+    subtitle: {
+        marginTop: 30,
+        fontSize: 18,
+        fontWeight: 'bold',
+        color: "#3D3F43"
+    },
+
+    separator: {
+        marginVertical: 10,
+        height: 1,
+        width: '80%',
+    },
+
+    scrollElem: {
+        marginVertical: 10,
+        justifyContent: 'center',
+        borderRadius: 8,
+        overflow: 'hidden',
+        shadowColor: "#000",
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        },
+        shadowOpacity: 0.30,
+        shadowRadius: 4.65,
+        
+        elevation: 8,
+    },
+
+    image: {
+        height: 0.9 * 1080 / scaleFactor,
+        width: 0.9 * Dimensions.get('window').width
+    },
+
+    evInfo: {
+        position: 'absolute',
+        top: 150,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        paddingLeft: 38,
+        paddingTop: 10,
+        backgroundColor: 'white'
+    },
+    evTitle: {
+        color: "#3D3F43",
+        fontWeight: '600',
+        fontSize: 18
+    },
+    evRoute:{
+        color: "#80848A",
+        fontSize: 12
+    }
+});
 
 const Buttons = ({navigation}) => {
     const buttons = [];
@@ -16,10 +104,26 @@ const Buttons = ({navigation}) => {
                     description: routes[i].description
                 })}>
                 <View style={styles.scrollElem}>
-                    <Image source={pic} style={styles.image}/>
+                    <MaskedView
+                        maskElement={
+                            <View
+                                style={{
+                                    width:  0.9 * Dimensions.get('window').width,
+                                    height: 150,
+                                    backgroundColor: 'black',
+                                    justifyContent: 'center',
+                                    alignItems: 'center',
+                                }}
+                            >
+                            </View>
+                        }
+                    >
+                        <Image source={pic} style={styles.image}/>
+                    </MaskedView>
+
                     <View style={styles.evInfo}>
-                        <Text style={styles.evView}>EV {routes[i].id}</Text>
-                        <Text style={styles.evView}>{routes[i].length} km</Text>
+                        <Text style={styles.evTitle}>{routes[i].title}</Text>
+                        <Text style={styles.evRoute}>Route {routes[i].id}</Text>
                     </View>
                 </View>
             </TouchableHighlight>
@@ -28,61 +132,33 @@ const Buttons = ({navigation}) => {
     return buttons;
 }
 
-const Roads = ({navigation}) => {
+
+const RoadsScreen = ({navigation}) => {
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Eurovelo</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)"/>
             <ScrollView>
+               <Text style={styles.title}>euro.<Text style={styles.velo}>Velo</Text></Text>
+               <Text style={styles.subtitle}>The Routes</Text>
                 {Buttons({navigation})}
             </ScrollView>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'flex-start',
-    },
+/*
+WRAPPER DE L'ECRAN AVEC TOUTES LES ROUTES POUR LA NAVIGATION<3
+*/
 
-    title: {
-        marginTop: 10,
-        fontSize: 50,
-        fontWeight: 'bold',
-    },
-
-    separator: {
-        marginVertical: 10,
-        height: 1,
-        width: '80%',
-    },
-    scrollElem: {
-        justifyContent: 'center'
-
-    },
-
-    image: {
-        height: 200,
-        width: 400,
-
-    },
-
-    evInfo: {
-        paddingTop: 10,
-        paddingBottom: 10,
-        flexDirection: 'row',
-        justifyContent: 'center',
-        backgroundColor: '#d3d3d3',
-    },
-
-    evView: {
-        fontSize: 30,
-        justifyContent: 'space-around',
-        paddingLeft: 50,
-        paddingRight: 80,
-    }
-});
+const RoadsScreenStackOptions = {
+    header: () => {return(<View></View>)} // NO header
+}
+const Roads = () => {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen name="Roads" component={RoadsScreen} options={RoadsScreenStackOptions}/>
+            <Stack.Screen name="Description" component={Description} />
+        </Stack.Navigator>
+    );
+}
 
 export default Roads;
