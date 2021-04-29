da2a189d5d083d9c058914c121f810a65e8cbcf2
inmisericors <46652188+inmisericors@users.noreply.github.com>
2021-04-29 13:22:52 +0200

    La db devrait fonctionner + bug de dates réglé

--- src/screens/OrganisationScreen/index.js
+++ src/screens/OrganisationScreen/index.js
@@ -4,23 +4,30 @@ import {CheckBox} from "react-native-elements";
 import pic from '../../assets/images/ev.jpg';
 
 
-
 export default class Screen extends Component {
     constructor(props) {
         super(props);
         this.state = {
             check: [],
             organisation_db: props.organisation_db,
-            date: this.getDateCode(),
+            date_codes: this.getDateCodes(),
             first_time: 1
         };
     }
 
-    getDateCode() {
-        const date = new Date().getDate();
-        const month = new Date().getMonth() + 1;
-        const year = new Date().getFullYear();
-        return year * 10000 + month * 100 + date;
+    getDateCodes() {
+        const today = new Date();
+        const tomorrow = new Date(today);
+        tomorrow.setDate(tomorrow.getDate() + 1);
+        const inTwoDays = new Date(today);
+        inTwoDays.setDate(inTwoDays.getDate() + 2);
+
+        const codes = [];
+        codes[0] = today.getFullYear() * 10000 + (today.getMonth() + 1) * 100 + today.getDate();
+        codes[1] = tomorrow.getFullYear() * 10000 + (tomorrow.getMonth() + 1) * 100 + tomorrow.getDate();
+        codes[2] = inTwoDays.getFullYear() * 10000 + (inTwoDays.getMonth() + 1) * 100 + inTwoDays.getDate();
+
+        return codes;
     }
 
     days = ["Today", "Tomorrow", "In two days"]
@@ -33,6 +40,7 @@ export default class Screen extends Component {
                     'UPDATE Check_list SET Breakfast = ? WHERE Date = ?;',
                     [yes_or_no, date],
                     (tx, results) => {
+                        // alert(results.rowsAffected + " line(s) in the database were changed.");
                     });
             });
         } else if (which_box === "Dinner") {
@@ -41,6 +49,7 @@ export default class Screen extends Component {
                     'UPDATE Check_list SET Dinner = ? WHERE Date = ?;',
                     [yes_or_no, date],
                     (tx, results) => {
+                        // alert(results.rowsAffected + " line(s) in the database were changed.");
                     });
             });
         } else if (which_box === "Supper") {
@@ -49,6 +58,7 @@ export default class Screen extends Component {
                     'UPDATE Check_list SET Supper = ? WHERE Date = ?;',
                     [yes_or_no, date],
                     (tx, results) => {
+                        // alert(results.rowsAffected + " line(s) in the database were changed.");
                     });
             });
         } else if (which_box === "Sleeping place") {
@@ -57,138 +67,137 @@ export default class Screen extends Component {
                     'UPDATE Check_list SET "Sleeping place" = ? WHERE Date = ?;',
                     [yes_or_no, date],
                     (tx, results) => {
+                        // alert(results.rowsAffected + " line(s) in the database were changed.");
                     });
             });
         }
 
     }
 
-    render() {
+    componentDidMount() {
         /* on va récupérer les checkboxes déjà cochées dans la db */
-        if (this.state.first_time) {
-            this.setState({first_time: 0});
+        this.state.organisation_db.transaction((tx) => {
+            tx.executeSql(
+                'DELETE FROM Check_list WHERE Date < ?;',
+                [this.state.date_codes[0]],
+                (tx, results) => {
+                }
+            );
+        });
 
+        for (let i = 0; i < 3; i++) {
             this.state.organisation_db.transaction((tx) => {
                 tx.executeSql(
-                    'DELETE FROM Check_list WHERE Date < ?;',
-                    [this.state.date],
+                    'SELECT * FROM Check_list WHERE Date == ?;',
+                    [this.state.date_codes[i]],
                     (tx, results) => {
+                        if (results.rows.length > 0) {
+                            let res = results.rows.item(0);
+                            const my_check = this.state.check;
+                            my_check[4 * i] = res["Breakfast"];
+                            my_check[4 * i + 1] = res["Dinner"];
+                            my_check[4 * i + 2] = res["Supper"];
+                            my_check[4 * i + 3] = res["Sleeping place"];
+
+
+                            this.setState({check: my_check})
+                        } else {
+                            this.state.organisation_db.transaction((tx) => {
+                                tx.executeSql(
+                                    'INSERT INTO "Check_list"\n' +
+                                    '("Breakfast", "Dinner", "Supper", "Sleeping place", "Date")\n' +
+                                    'VALUES (0, 0, 0, 0, ?);',
+                                    [this.state.date_codes[i]],
+                                    (tx, results) => {
+                                    }
+                                );
+                            });
+                        }
                     }
                 );
             });
-
-            for (let i = 0; i < 3; i++) {
-                this.state.organisation_db.transaction((tx) => {
-                    tx.executeSql(
-                        'SELECT * FROM Check_list WHERE Date == ?;',
-                        [this.state.date + i],
-                        (tx, results) => {
-                            if (results.rows.length > 0) {
-                                let res = results.rows.item(0);
-                                const my_check = this.state.check;
-                                my_check[4 * i] = res["Breakfast"];
-                                my_check[4 * i + 1] = res["Dinner"];
-                                my_check[4 * i + 2] = res["Supper"];
-                                my_check[4 * i + 3] = res["Sleeping place"];
-
-
-                                this.setState({check: my_check})
-                            } else {
-                                this.state.organisation_db.transaction((tx) => {
-                                    tx.executeSql(
-                                        'INSERT INTO "Check_list"\n' +
-                                        '("Breakfast", "Dinner", "Supper", "Sleeping place", "Date")\n' +
-                                        'VALUES (0, 0, 0, 0, ?);',
-                                        [this.state.date + i],
-                                        (tx, results) => {
-                                        }
-                                    );
-                                });
-                            }
-                        }
-                    );
-                });
-            }
         }
+    }
 
-        // on crée les checkboxes
-        const Buttons = () => {
-            const buttons = [];
-                for (let i = 0; i < 3; i++) {
-                    buttons.push(
-                        <View key={i}>
-                            <Text style={styles.second_title}>{this.days[i]}</Text>
+    // on crée les checkboxes
+    Buttons() {
+        const buttons = [];
+        for (let i = 0; i < 3; i++) {
+            buttons.push(
+                <View key={i}>
+                    <Text style={styles.second_title}>{this.days[i]}</Text>
 
-                            <View style={styles.row}>
-                                    <View style={styles.checkbox}>
-                                        <CheckBox
-                                            title='Breakfast'
-                                            checkedColor="#2FD175"
-                                            checked={this.state.check[4 * i]} // 4*i pour atteindre la bonne checkbox
-                                            textStyle={styles.textButton}
-                                            onPress={() => {
-                                                let my_check = this.state.check;
-                                                my_check[4 * i] = !my_check[4 * i];
-                                                this.setState({check: my_check})
-                                                this.setCheck("Breakfast", my_check[4 * i], this.state.date + i);
-                                            }}
-                                        />
-                                    </View>
-                                    <View style={styles.checkbox}>
-                                        <CheckBox
-                                            title='Dinner'
-                                            checkedColor="#2FD175"
-                                            checked={this.state.check[4 * i + 1]}
-                                            textStyle={styles.textButton}
-                                            onPress={() => {
-                                                let my_check = this.state.check;
-                                                my_check[4 * i + 1] = !my_check[4 * i + 1];
-                                                this.setState({check: my_check});
-                                                this.setCheck("Dinner", my_check[4 * i + 1], this.state.date + i);
-                                            }}
-                                        />
-                                    </View>
-                                </View>
-
-
-                            <View style={styles.row}>
-                                    <View style={styles.checkbox}>
-                                        <CheckBox
-                                            title='Supper'
-                                            checkedColor="#2FD175"
-                                            checked={this.state.check[4 * i + 2]} // 4*i pour atteindre la bonne checkbox
-                                            textStyle={styles.textButton}
-                                            onPress={() => {
-                                                let my_check = this.state.check;
-                                                my_check[4 * i + 2] = !my_check[4 * i + 2];
-                                                this.setState({check: my_check})
-                                                this.setCheck("Supper", my_check[4 * i + 2], this.state.date + i);
-                                            }}
-                                        />
-                                    </View>
-                                    <View style={styles.checkbox}>
-                                        <CheckBox
-                                            title='Sleeping place'
-                                            checkedColor="#2FD175"
-                                            checked={this.state.check[4 * i + 3]}
-                                            containerStyle={styles.Button}
-                                            textStyle={styles.textButton}
-                                            onPress={() => {
-                                                let my_check = this.state.check;
-                                                my_check[4 * i + 3] = !my_check[4 * i + 3];
-                                                this.setState({check: my_check});
-                                                this.setCheck("Sleeping place", my_check[4 * i + 3], this.state.date + i);
-                                            }}
-                                        />
-                                    </View>
-                            </View>
-
+                    <View style={styles.row}>
+                        <View style={styles.checkbox}>
+                            <CheckBox
+                                title='Breakfast'
+                                checkedColor="#2FD175"
+                                checked={this.state.check[4 * i]} // 4*i pour atteindre la bonne checkbox
+                                textStyle={styles.textButton}
+                                onPress={() => {
+                                    let my_check = this.state.check;
+                                    my_check[4 * i] = !my_check[4 * i];
+                                    this.setState({check: my_check})
+                                    this.setCheck("Breakfast", my_check[4 * i], this.state.date_codes[i]);
+                                }}
+                            />
                         </View>
-                    );
-                }
-            //
-            return buttons;
-        };
+                        <View style={styles.checkbox}>
+                            <CheckBox
+                                title='Dinner'
+                                checkedColor="#2FD175"
+                                checked={this.state.check[4 * i + 1]}
+                                textStyle={styles.textButton}
+                                onPress={() => {
+                                    let my_check = this.state.check;
+                                    my_check[4 * i + 1] = !my_check[4 * i + 1];
+                                    this.setState({check: my_check});
+                                    this.setCheck("Dinner", my_check[4 * i + 1], this.state.date_codes[i]);
+                                }}
+                            />
+                        </View>
+                    </View>
+
+
+                    <View style={styles.row}>
+                        <View style={styles.checkbox}>
+                            <CheckBox
+                                title='Supper'
+                                checkedColor="#2FD175"
+                                checked={this.state.check[4 * i + 2]} // 4*i pour atteindre la bonne checkbox
+                                textStyle={styles.textButton}
+                                onPress={() => {
+                                    let my_check = this.state.check;
+                                    my_check[4 * i + 2] = !my_check[4 * i + 2];
+                                    this.setState({check: my_check})
+                                    this.setCheck("Supper", my_check[4 * i + 2], this.state.date_codes[i]);
+                                }}
+                            />
+                        </View>
+                        <View style={styles.checkbox}>
+                            <CheckBox
+                                title='Sleeping place'
+                                checkedColor="#2FD175"
+                                checked={this.state.check[4 * i + 3]}
+                                containerStyle={styles.Button}
+                                textStyle={styles.textButton}
+                                onPress={() => {
+                                    let my_check = this.state.check;
+                                    my_check[4 * i + 3] = !my_check[4 * i + 3];
+                                    this.setState({check: my_check});
+                                    this.setCheck("Sleeping place", my_check[4 * i + 3], this.state.date_codes[i]);
+                                }}
+                            />
+                        </View>
+                    </View>
+                </View>
+            );
+        }
+        //
+        return buttons;
+    };
+
+    render() {
 
         return (
             <SafeAreaView style={styles.container}>
@@ -196,7 +205,7 @@ export default class Screen extends Component {
                     <Text style={styles.title}>Organisation</Text>
                 </View>
                 <View style={styles.little_second}>
-                    {Buttons()}
+                    {this.Buttons()}
                 </View>
                 <View style={styles.little_third}>
                     <Image source={pic} style={styles.image} resizeMode={'cover'}/>
@@ -218,14 +227,13 @@ const styles = StyleSheet.create({
     },
 
     title: {
-        paddingTop:'3%',
+        paddingTop: '3%',
         paddingHorizontal: '5%',
-        fontSize: Dimensions.get("window").height*0.055,
+        fontSize: Dimensions.get("window").height * 0.055,
         fontWeight: 'bold',
-        color:"#2FD175",
+        color: "#2FD175",
     },
-    little_first: {
-    },
+    little_first: {},
     little_second: {
         marginBottom: '5%'
     },
@@ -236,7 +244,7 @@ const styles = StyleSheet.create({
         marginBottom: 0,
     },
     second_title: {
-        fontSize: Dimensions.get("window").height*0.021,
+        fontSize: Dimensions.get("window").height * 0.021,
         fontWeight: 'bold',
         paddingLeft: '5%',
         marginTop: '2%',
@@ -255,15 +263,15 @@ const styles = StyleSheet.create({
     },
     image: {
 
-        direction:'ltr',
+        direction: 'ltr',
         borderRadius: 30,
-        overflow:'hidden',
-        bottom:0,
+        overflow: 'hidden',
+        bottom: 0,
         height: '100%',
         width: '100%',
         marginTop: '-2%',
     },
     textButton: {
-        fontSize: Dimensions.get("window").height*0.019,
+        fontSize: Dimensions.get("window").height * 0.019,
     }
 });
